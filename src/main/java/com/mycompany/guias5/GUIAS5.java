855af86cd3e806d675ef0f5039fc1c5bf1be06e5
NadirMarsiglia <nadir.marsigliababilonia@unicolombo.edu.co>
2024-04-21 18:53:48 -0500

    primer GUIA5

---
+++ src/main/java/com/mycompany/guias5/GUIAS5.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guias5;
+
+import igui.pantallas5;
+
+/**
+ *
+ * @author nadir
+ */
+public class GUIAS5 {
+
+    public static void main(String[] args) {
+       new pantallas5().setVisible(true);
+    }
+}
